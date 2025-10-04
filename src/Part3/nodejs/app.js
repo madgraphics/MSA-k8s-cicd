a4d267bacc394a597551e6d6792875623728ae8b
technomad <33218236+dontotl@users.noreply.github.com>
2025-10-04 18:35:25 +0900

    Create app.js

---
+++ src/Part3/nodejs/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const port = 3000;
+
+app.get('/v1/manage/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/v1/manage/', (req, res) => {
+  var data, data2
+  request('http://192.168.49.2:31971/v1/user/', function (error, response, body) {
+          if(!error && response.statusCode == 200) {
+                  data = JSON.stringify(body);
+                  console.log(data);
+          }
+  });
+  request('http://192.168.49.2:32281/v1/movies/', function (error, response, body) {
+          if(!error && response.statusCode == 200) {
+                  data2 = JSON.stringify(body);
+                  console.log(data2);
+                  res.send(data.concat(' ',data2));
+          }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening port : ${port}`);
+});
