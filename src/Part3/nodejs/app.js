40baeedc0a17e42bd02a9638b926f383e76e595b
technomad <33218236+dontotl@users.noreply.github.com>
2025-10-04 18:36:58 +0900

    Update app.js

--- src/Part3/nodejs/app.js
+++ src/Part3/nodejs/app.js
@@ -3,6 +3,11 @@ const app = express();
 const request = require('request');
 const port = 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.get('/v1/manage/hello', (req, res) => {
   res.send('Hello World!');
 });
@@ -10,17 +15,17 @@ app.get('/v1/manage/hello', (req, res) => {
 app.get('/v1/manage/', (req, res) => {
   var data, data2
   request('http://192.168.49.2:31971/v1/user/', function (error, response, body) {
-          if(!error && response.statusCode == 200) {
-                  data = JSON.stringify(body);
-                  console.log(data);
-          }
+	  if(!error && response.statusCode == 200) {
+		  data = JSON.stringify(body);
+		  console.log(data);
+	  }
   });
   request('http://192.168.49.2:32281/v1/movies/', function (error, response, body) {
-          if(!error && response.statusCode == 200) {
-                  data2 = JSON.stringify(body);
-                  console.log(data2);
-                  res.send(data.concat(' ',data2));
-          }
+	  if(!error && response.statusCode == 200) {
+		  data2 = JSON.stringify(body);
+		  console.log(data2);
+		  res.send(data.concat(' ',data2));
+	  }
   });
 });
 
